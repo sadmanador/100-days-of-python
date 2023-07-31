104eda630142c1dad0828f67b2db33ed70bb2296
sadmanador <sadman.ador@gmail.com>
2023-07-31 17:52:53 +0600

    day-2 problem solved

---
+++ day-2/video_20.py
@@ -0,0 +1,4 @@
+num_char = len(input("What is you name? "))
+print("You name has " + str(num_char) + " numbers of character.")
+
+#only string can be concat together | to overcome the problem type conversion is need
---
+++ day-2/video_21.py
@@ -0,0 +1,6 @@
+#practice problem
+int_to_addition = input("enter 2 digit number, get an addition ")
+first_digit = int(int_to_addition[0])
+second_digit = int(int_to_addition[1])
+total = first_digit + second_digit
+print(total)
