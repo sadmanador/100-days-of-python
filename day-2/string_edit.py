4848c2ca038d8458b642592cb674da5c553b5fcc
sadmanador <sadman.ador@gmail.com>
2023-07-31 17:37:59 +0600

    video 19 done

---
+++ day-2/string_edit.py
@@ -0,0 +1,27 @@
+#data types
+
+#string
+str = "hello"
+# print(str[0])
+# print(str[1])
+# print(str[2])
+# print(str[3])
+# print(str[4])
+
+#concat
+str1 = 'hello'
+str2 = ' world'
+print(str1 + str2)
+
+
+#large numbers can be written like below instead of ,
+large_number = 123_456_789
+print(large_number)
+
+#float
+pi = 3.1415
+print(type(pi))
+
+
+isRaining = False
+print(isRaining)
