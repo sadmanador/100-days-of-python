d2d647e69051bfb028a4ca35571126c677dc90f7
sadmanador <sadman.ador@gmail.com>
2023-11-04 19:15:41 +0600

    video 29 done

---
+++ day-3/video-29.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is you height in CM? "))
+
+if height >= 120:
+    print("Enjoy the ride!!!")
+else:
+    print("Sorry, you're not allow to ride")
