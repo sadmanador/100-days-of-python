2f48c7af1211633d43a56b8ba99a78d2df3c783c
sadmanador <sadman.ador@gmail.com>
2023-11-04 19:29:42 +0600

    nested condition done

--- day-3/video-29.py
+++ day-3/video-29.py
@@ -2,6 +2,10 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is you height in CM? "))
 
 if height >= 120:
-    print("Enjoy the ride!!!")
+    age = int(input("How old are you? "))
+    if age <= 18:
+        print("You have to pay $7")
+    else:
+        print("You have to pay $12")
 else:
     print("Sorry, you're not allow to ride")
---
+++ day-3/video-30.py
@@ -0,0 +1,7 @@
+print("Welcome to the odd even finder")
+userInput = int(input("Give a number "))
+
+if userInput % 2 == 0:
+    print("Your given number is EVEN")
+else:
+    print("Your given number is ODD")
